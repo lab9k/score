b8351c0c579222515c105e09afe2f2dc9d0ce268
Jefwillems <jefwillems100@gmail.com>
2018-03-06 13:45:54 +0100

    add gulp

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require("gulp");
+var useref = require("gulp-useref");
+var gulpif = require("gulp-if");
+var minifyCss = require("gulp-clean-css");
+var uglify = require("gulp-uglify");
+
+gulp.task("build", function() {
+  return gulp
+    .src("./index.html")
+    .pipe(useref())
+    .pipe(gulpif("*.css", minifyCss()))
+    .pipe(gulpif("*.js"), uglify())
+    .pipe(gulp.dest("./docs/"));
+});
