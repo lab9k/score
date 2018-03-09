3d5e40e5555041f0c6fa3c1c5b0b30bae360902d
Jefwillems <jefwillems100@gmail.com>
2018-03-09 18:40:02 +0100

    disabled uglify

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require("gulp");
 var useref = require("gulp-useref");
 var gulpif = require("gulp-if");
 var minifyCss = require("gulp-clean-css");
-var uglify = require("gulp-uglify");
+//var uglify = require("gulp-uglify");
 
 gulp.task("assets", function() {
   return gulp.src("./src/flare.json").pipe(gulp.dest("./docs/src/"));
