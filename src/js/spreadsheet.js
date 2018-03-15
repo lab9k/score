e0d23a4d1e78a834be10e511fb6d6625561c6890
Jefwillems <jefwillems100@gmail.com>
2018-03-15 17:34:17 +0100

    parsing data in a different way

--- src/js/spreadsheet.js
+++ src/js/spreadsheet.js
@@ -4,7 +4,7 @@ function SpreadsheetDataService() {
 }
 
 SpreadsheetDataService.prototype.fetch = function(cb) {
-  fetchJson(this.url, function(raw_data) {
+  /*fetchJson(this.url, function(raw_data) {
     var parsed_json = Object.create({});
     if (raw_data.feed && raw_data.feed.entry) {
       parsed_json["name"] = "score";
@@ -25,6 +25,54 @@ SpreadsheetDataService.prototype.fetch = function(cb) {
       });
     }
     console.log("Parsed JSON: ", parsed_json);
+    cb(parsed_json);
+  });*/
+  fetchJson(this.url, function(raw_data) {
+    var parsed_json = Object.create({});
+    if (raw_data.feed && raw_data.feed.entry) {
+      parsed_json["name"] = "score";
+      parsed_json["children"] = [];
+      var rows = raw_data.feed.entry;
+      rows.forEach(row => {
+        /*
+        var kwChildren = main.children[THEME].children[KEYWORD].children.push({name:title,...})
+        */
+
+        var themeValue = row["gsx$theme"]["$t"];
+        var kwValue = row["gsx$keyword"]["$t"];
+        var titleValue = row["gsx$title"]["$t"];
+        var descriptionValue = row["gsx$description"]["$t"];
+        var contactValue = row["gsx$contact"]["$t"];
+        var cityValue = row["gsx$city"]["$t"];
+
+        var rowObj = {
+          name: titleValue,
+          description: descriptionValue,
+          contact: contactValue,
+          city: cityValue,
+          size: 256
+        };
+
+        var themeObj = parsed_json.children.find(el => {
+          return el.name === themeValue;
+        });
+        if (!themeObj) {
+          themeObj = { name: themeValue, children: [] };
+          themeObj.children.push({ name: kwValue, children: [rowObj] });
+          parsed_json.children.push(themeObj);
+        } else {
+          var kwObj = themeObj.children.find(el => {
+            return el.name === kwValue;
+          });
+          if (!kwObj) {
+            themeObj.children.push({ name: kwValue, children: [rowObj] });
+            parsed_json.children.push(themeObj);
+          } else {
+            kwObj.children.push(rowObj);
+          }
+        }
+      });
+    }
     cb(parsed_json);
   });
 };
